f3a23dc934781c217945810bdc118f418b50aebe
flawnson <flawnsontong1@gmail.com>
2022-09-26 15:39:08 -0400

    Created basic login and sign in page, installed react navigation

---
+++ hunt-and-gather/screens/SplashScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+    HStack,
+    Center,
+} from "native-base";
+import NativeBaseIcon from "../components/NativeBaseIcon";
+
+export default function App() {
+    return (
+        <Center>
+            <HStack>
+                <NativeBaseIcon />
+            </HStack>
+        </Center>
+    )
+}
